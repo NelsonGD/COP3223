420791ee855bf7ed12c957a80e5cae817000febb
Nelson <nelsongael.diaz@gmail.com>
2024-02-19 15:52:11 -0500

    Folder was cloned twice inside of itself for some reason

--- assignment/Problem-4DigitNumbers/main.h
+++ assignment/Problem-4DigitNumbers/main.h
@@ -1,11 +1,5 @@
-// Function 1
 int isFourDigit(int num);
 
-// Function 2
 int reverse4Digit(int rnum);
 
-// Function 3
 int addSides4Digit(int num);
-
-// Function 4
-int isPalindrome4Digit(int num);
