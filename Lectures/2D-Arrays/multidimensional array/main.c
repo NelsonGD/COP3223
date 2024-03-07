ff261f01f19d24ac3ac548d5e59f3b9b386ee16c
Nelson <nelsongael.diaz@gmail.com>
2024-03-07 15:08:43 -0500

    stuyding and notes

---
+++ Lectures/2D-Arrays/multidimensional array/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int st[4][2];
+
+    for(int i=0; i<4; i++)
+    {
+
+    }
+
+
+    printf("Student list\n====\n");
+
+    printf("ID\tGrade\n====\n");
+    for(int i=0; i<4; i++)
+    {
+        printf("");
+        scanf("%d", &);
+    }
+
+    return 0;
+}
